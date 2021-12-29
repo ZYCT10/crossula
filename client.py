bef9787d612281172ac89482f22e495396b1a03b
Maksim <92832262+ZYCT10@users.noreply.github.com>
2021-12-29 19:39:59 +0300

    Version 1.0.3

--- client.py
+++ client.py
@@ -11,21 +11,29 @@ class Client:
             "Authorization": self.authKey
         }
 
-    def __send_request(self, request_url, method="GET", data={}, params={}, headers=None):
+    def __send_request(self, request_url, method="GET", data={}, headers=None):
         if headers is None:
             headers = self.headers
         
         if method == "GET":
-            r = requests.get(f"{self.api_url}/{request_url}", params=params, headers=headers)
-        elif method == "POST":
-            r = requests.post(f"{self.api_url}/{request_url}", headers=headers, data=data)
-        elif method == "PUT":
-            r = requests.put(f"{self.api_url}/{request_url}", headers=headers, data=data)
+            r = requests.get(f"{self.api_url}/{request_url}", headers=headers, params=data)
         
+        else:
+            headers["Content-Type"] = "application/json"
+
+            if type(data) != dict:
+                    data = json.loads(data)
+            
+            if method == "POST":
+                r = requests.post(f"{self.api_url}/{request_url}", headers=headers, data=json.dumps(data))
+
+            elif method == "PUT":
+                r = requests.put(f"{self.api_url}/{request_url}", headers=headers, data=json.dumps(data))
+            
         if r.status_code != 200:
             raise CrossulaException(f"{r.status_code}")
 
-        return r.text
+        return json.loads(r.text)
 
 
     """ Create an account (Subaccount) """
@@ -33,47 +41,61 @@ class Client:
         return self.__send_request(f"api/clients/{self.clientId}/accounts", method="POST", data=params)
 
     """ Close account (Subaccount) """
-    def close_account(self, accountId):
-        return self.__send_request(f"api/clients/{self.clientId}/accounts/{accountId}/close", method="PUT")
+    def close_account(self, accountId, params={}):
+        return self.__send_request(f"api/clients/{self.clientId}/accounts/{accountId}/close", method="PUT", data=params)
 
     """ Get account (Subaccount) information """
-    def get_account_info(self, accountId):
-        return self.__send_request(f"api/clients/{self.clientId}/accounts/{accountId}")
+    def get_account_info(self, accountId, params={}):
+        return self.__send_request(f"api/clients/{self.clientId}/accounts/{accountId}", data=params)
 
     """ Create account (Subaccount) IBAN """
-    def create_account_iban(self, accountId):
-        return self.__send_request(f"api/clients/{self.clientId}/accounts/{accountId}/iban", method="POST")
+    def create_account_iban(self, accountId, params={}):
+        return self.__send_request(f"api/clients/{self.clientId}/accounts/{accountId}/iban", method="POST", data=params)
     
     """ Get account (Subaccount) IBAN list """
-    def get_account_ibans(self, accountId):
-        return self.__send_request(f"api/clients/{self.clientId}/accounts/{accountId}/iban")
+    def get_account_ibans(self, accountId, params={}):
+        return self.__send_request(f"api/clients/{self.clientId}/accounts/{accountId}/iban", data=params)
 
-    """ Get account (Subaccount) IBAN Information """
-    def get_account_iban_info(self, accountId, accountIBAN):
-        return self.__send_request(f"api/clients/{self.clientId}/accounts/{accountId}/iban/{accountIBAN}/balances")
+    def get_account_balances(self, params={}):
+        return self.__send_request(f"api/clients/{self.clientId}/balances", data=params)
+
+    """ Get account (Subaccount) IBAN balances """
+    def get_account_iban_balances(self, accountId, accountIBAN, params={}):
+        return self.__send_request(f"api/clients/{self.clientId}/accounts/{accountId}/iban/{accountIBAN}/balances", data=params)
+
+    """ Get transactions by transactionID """
+    def get_transaction_by_id(self, transactionId, params={}):
+        return self.__send_request(f"api/clients/{self.clientId}/transactions/{transactionId}", data=params)
 
     """ Currency exchange """
-    def account_exchange(self, body):
-        return self.__send_request(f"api/clients/{self.clientId}/currency/exchange", method="POST", data=body)
+    def account_exchange(self, params):
+        return self.__send_request(f"api/clients/{self.clientId}/currency/exchange", method="POST", data=params)
+
+    """ Make internal transfer """
+    def make_internal_transfer(self, XConfirmationCode, params):
+        headers = self.headers.copy()
+        headers["X-Confirmation-Code"] = str(XConfirmationCode)
+
+        return self.__send_request(f"api/clients/{self.clientId}/transfer/internal", method="POST", data=params, headers=headers)
 
     """ Make sepa transfer """
-    def make_sepa_transfer(self, XConfirmationCode, body):
+    def make_sepa_transfer(self, XConfirmationCode, params):
         headers = self.headers.copy()
-        headers["X-Confirmation-Code"] = XConfirmationCode
+        headers["X-Confirmation-Code"] = str(XConfirmationCode)
 
-        return self.__send_request(f"api/clients/{self.clientId}/transfer/sepa", method="POST", data=body, headers=headers)
+        return self.__send_request(f"api/clients/{self.clientId}/transfer/sepa", method="POST", data=params, headers=headers)
 
     """ Make swift transfer """    
-    def make_swift_transfer(self, XConfirmationCode, body):
+    def make_swift_transfer(self, XConfirmationCode, params):
         headers = self.headers.copy()
-        headers["X-Confirmation-Code"] = XConfirmationCode
+        headers["X-Confirmation-Code"] = str(XConfirmationCode)
 
-        return self.__send_request(f"api/clients/{self.clientId}/transfer/swift", method="POST", data=body, headers=headers)
+        return self.__send_request(f"api/clients/{self.clientId}/transfer/swift", method="POST", data=params, headers=headers)
 
     """ Get transaction list """
     def get_transaction_list(self, params={}):
-        return self.__send_request(f"api/clients/{self.clientId}/transactions", params=params)
+        return self.__send_request(f"api/clients/{self.clientId}/transactions", data=params)
 
     """ Get account (Subaccount) list """
-    def get_account_list(self):
-        return self.__send_request(f"api/clients/{self.clientId}/accounts")
+    def get_account_list(self, params={}):
+        return self.__send_request(f"api/clients/{self.clientId}/accounts", data=params)
